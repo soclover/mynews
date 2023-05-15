7e5fa4c68ae9223fb8ee016042427151d598bc96
Shinsho <soclover@gmail.com>
2023-05-15 09:43:13 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
     Route::get('profile/create', 'add')->name('profile.add');
     Route::get('profile/edit', 'edit')->name('profile.edit');
-    Route::post('profile/create', 'add')->name('profile.create');
+    Route::post('profile/create', 'create')->name('profile.create');
     Route::post('profile/edit', 'update')->name('profile.edit');
 });
 Auth::routes();
